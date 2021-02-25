a8f0dbbd0596bab148b92fdfdb1022350a7a187f
Sandro Carvalho <sandrinho9797@outlook.com>
2021-02-24 23:26:40 -0300

    first commit

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,9 @@
+import { Entity, EntityRepository, Repository } from "typeorm";
+import { User } from "../models/User";
+
+@EntityRepository(User)
+class UsersRespository extends Repository<User> {
+
+}
+
+export { UsersRespository }
