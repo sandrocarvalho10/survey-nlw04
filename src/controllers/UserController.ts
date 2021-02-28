1e55e9c19227dbf4d03170c327a2a8d3017bf0d7
Sandro Carvalho <sandrinho9797@outlook.com>
2021-02-28 18:05:53 -0300

    Aula 5

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,17 +6,17 @@ import { AppError } from '../errors/AppError';
 
 class UserController {
 
-  async create(request: Request, response: Response){
+  async create(request: Request, response: Response) {
     const { name, email } = request.body;
 
     const schema = yup.object().shape({
       name: yup.string().required(),
       email: yup.string().email().required()
-    })    
+    })
 
-    try{
+    try {
       await schema.validate(request.body, { aboutEarly: false });
-    }catch(err){
+    } catch (err) {
       throw new AppError(err)
     }
 
@@ -26,7 +26,7 @@ class UserController {
       email
     })
 
-    if(userAlreadyExists) {
+    if (userAlreadyExists) {
       throw new AppError("User already exists!")
     }
 
@@ -35,7 +35,7 @@ class UserController {
     })
 
     await usersRepository.save(user)
-    
+
     return response.status(201).json(user)
   }
 
